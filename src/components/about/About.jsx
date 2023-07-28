d13b49ab75a911f69331f398780592eb44e282bc
Pankaj Wakchaure <pankaj.corp@gmail.com>
2023-07-28 21:12:45 +0530

    Code splitting example added.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import aboutStyles from "./about.css";
 
 export default function About() {
-  return <h3 className={aboutStyles.color} >About</h3>;
+  return <h3 className={aboutStyles.color} >About Page</h3>;
 }
