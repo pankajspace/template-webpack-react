0999c9937fd36bd1aaf2dc5e463c0a9129555d21
Pankaj Wakchaure <pankaj.corp@gmail.com>
2023-07-28 19:29:58 +0530

    React code refactoring done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
-
 import { createRoot } from 'react-dom/client';
+
 import App from "./App";
 
-const container = document.getElementById('react-app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+createRoot(document.getElementById('react-app')).render(<App />);
 
 
 
